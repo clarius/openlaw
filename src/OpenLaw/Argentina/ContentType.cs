f206e8bd73637136e13a25fa770cb63c2c8e0bc5
Daniel Cazzulino <daniel@cazzulino.com>
2025-03-17 16:54:09 -0300

    Don't process news, only actual legislation
    News do not contain norm text actually, so there's no point in processing them.

--- src/OpenLaw/Argentina/ContentType.cs
+++ src/OpenLaw/Argentina/ContentType.cs
@@ -3,7 +3,7 @@
 public enum ContentType
 {
     Legislacion,
-    Novedad,
+    //Novedad, // Novedades tipicamente no contienen ningun texto de ninguna norma
     // Other content-types we don't currently support are:
     // Fallo
     // Sumario
