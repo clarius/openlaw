68e90adbb0938783afcf2d286c218b4780d5608e
Daniel Cazzulino <daniel@cazzulino.com>
2025-03-28 20:47:11 -0300

    Add sync details to markdown report
    If the sync action for a document was a non-trivial change (i.e. not just timestamps updating), emit details of the updated docs.

--- src/OpenLaw/Argentina/SyncSummary.cs
+++ src/OpenLaw/Argentina/SyncSummary.cs
@@ -1,9 +1,12 @@
 ﻿using System.Diagnostics;
+using System.Text;
+using DiffPlex.DiffBuilder;
 
 namespace Clarius.OpenLaw.Argentina;
 
 public class SyncSummary(string operation)
 {
+    readonly List<SyncActionResult> results = [];
     readonly HashSet<string> errors = [];
     readonly Stopwatch stopwatch = Stopwatch.StartNew();
     int created = 0;
@@ -19,11 +22,11 @@ public class SyncSummary(string operation)
 
     public TimeSpan Elapsed => stopwatch.Elapsed;
 
-    public static SyncSummary Start(string operation) => new SyncSummary(operation);
+    public static SyncSummary Start(string operation) => new(operation);
 
-    public void Add(ContentAction action)
+    public void Add(SyncActionResult result)
     {
-        switch (action)
+        switch (result.Action)
         {
             case ContentAction.Created:
                 Interlocked.Increment(ref created);
@@ -35,6 +38,8 @@ public class SyncSummary(string operation)
                 Interlocked.Increment(ref skipped);
                 break;
         }
+
+        results.Add(result);
     }
 
     public void Add(Exception? exception)
@@ -56,6 +61,58 @@ public class SyncSummary(string operation)
         if (Skipped > 0) report += $":white_check_mark: {Skipped} ";
         if (Failed > 0) report += $":x: {Failed} ";
 
-        return report + $":hourglass: {Elapsed.ToMinimalString()}";
+        report += $":hourglass: {Elapsed.ToMinimalString()}";
+
+        var details = new StringBuilder();
+
+        // TODO: add/count diff.
+        foreach (var result in results)
+        {
+            // quickly skip from report the dummy updates
+            if (result.Action == ContentAction.Skipped)
+                continue;
+            if (result.Action == ContentAction.Updated &&
+                result.OldDocument != null)
+            {
+                var diff = InlineDiffBuilder.Diff(result.OldDocument!.Json, result.NewDocument.Json)
+                    .Lines.Where(x => x.Type != DiffPlex.DiffBuilder.Model.ChangeType.Unchanged)
+                    .ToList();
+
+                if (diff.Count == 4 &&
+                    diff[0].Type == DiffPlex.DiffBuilder.Model.ChangeType.Deleted && diff[0].Text.Trim().StartsWith("\"timestamp\":") &&
+                    diff[1].Type == DiffPlex.DiffBuilder.Model.ChangeType.Inserted && diff[1].Text.Trim().StartsWith("\"timestamp\":") &&
+                    diff[2].Type == DiffPlex.DiffBuilder.Model.ChangeType.Deleted && diff[2].Text.Trim().StartsWith("\"fecha-umod\":") &&
+                    diff[3].Type == DiffPlex.DiffBuilder.Model.ChangeType.Inserted && diff[3].Text.Trim().StartsWith("\"fecha-umod\":"))
+                {
+                    // discard changes that are only timestamp/fecha-umod
+                    // see: https://github.com/clarius/normas/pull/32/files#diff-2f592ca38476012d2be4d6b3f17789b83b8bd3c3fa1df6eda2e54b8ccc7e1cbc
+                    continue;
+                }
+            }
+
+            details.AppendLine($"{(result.Action == ContentAction.Created ? ":heavy_plus_sign:" : ":pencil:")} {result.NewDocument.Name}: {result.NewDocument.Title}");
+        }
+
+        if (details.Length > 0)
+        {
+            report +=
+                """
+
+
+                <details>
+
+                <summary>:eyes: Detalles</summary>
+
+                """;
+            report += details.ToString();
+            report +=
+                """
+
+
+                </details>
+                """;
+        }
+
+        return report;
     }
 }
