f51cbcb7e5a120fac6d1c9c3851bf862578b9e0f
Daniel Cazzulino <daniel@cazzulino.com>
2025-03-28 20:47:11 -0300

    Add sync details to markdown report
    If the sync action for a document was a non-trivial change (i.e. not just timestamps updating), emit details of the updated docs.

---
+++ src/Tests/Argentina/SyncSummaryTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Clarius.OpenLaw.Argentina;
+
+public class SyncSummaryTests(ITestOutputHelper output)
+{
+    [Fact]
+    public async Task DiffOmitsTimestampChanges()
+    {
+        var newDoc = await Document.ParseAsync(File.ReadAllText(@$"../../../Argentina/SaijSamples/Diff/123456789-0abc-095-0000-7991soterced-new.json"));
+        var oldDoc = await Document.ParseAsync(File.ReadAllText(@$"../../../Argentina/SaijSamples/Diff/123456789-0abc-095-0000-7991soterced-old.json"));
+
+        var result = new SyncActionResult(ContentAction.Updated, newDoc, oldDoc);
+        var summary = new SyncSummary("Testing");
+        summary.Add(result);
+
+        var markdown = summary.ToMarkdown();
+
+        Assert.DoesNotContain("<details>", markdown);
+    }
+
+    [Fact]
+    public async Task DiffIncludesChangedDoc()
+    {
+        var newDoc = await Document.ParseAsync(File.ReadAllText(@$"../../../Argentina/SaijSamples/Diff/123456789-0abc-766-1000-2102soterced-new.json"));
+        var oldDoc = await Document.ParseAsync(File.ReadAllText(@$"../../../Argentina/SaijSamples/Diff/123456789-0abc-766-1000-2102soterced-old.json"));
+
+        var result = new SyncActionResult(ContentAction.Updated, newDoc, oldDoc);
+        var summary = new SyncSummary("Testing");
+        summary.Add(result);
+
+        var markdown = summary.ToMarkdown();
+
+        Assert.Contains("<details>", markdown);
+    }
+}
