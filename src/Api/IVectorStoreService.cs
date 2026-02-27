6a487dacc0c9be5972b2f54aa5f86c855d9e9533
Daniel Cazzulino <daniel@cazzulino.com>
2026-02-27 23:41:45 +0100

    Remove unused code

--- src/Api/IVectorStoreService.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace Clarius.OpenLaw
-{
-    public interface IVectorStoreService
-    {
-        Task<VectorStore?> GetStoreAsync(DateOnly documentDate, CancellationToken cancellation = default);
-        Task<IEnumerable<VectorStore>> GetStores(CancellationToken cancellation = default);
-    }
-}
